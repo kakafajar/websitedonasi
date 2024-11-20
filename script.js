c266f4b0eef9b62e0998c6c372bc626f029e0944
kakafajar <kakafajar1511@gmail.com>
2024-11-20 14:55:22 +0700

    form donasi
    Signed-off-by: kakafajar <kakafajar1511@gmail.com>

---
+++ script.js
@@ -0,0 +1,26 @@
+AOS.init({
+    duration: 2000, // Durasi animasi dalam milidetik
+    once: true // Animasi hanya berjalan sekali
+  });
+
+
+// js buat jumlah donasi
+
+document.addEventListener('DOMContentLoaded', function () {
+  const donasiLainnya = document.getElementById('donasiLainnya');
+  const inputLainnya = document.getElementById('inputLainnya');
+  const jumlahManual = document.getElementById('jumlah_manual');
+
+  document.querySelectorAll('input[name="jumlah_donasi"]').forEach(radio => {
+    radio.addEventListener('change', function () {
+      if (donasiLainnya.checked) {
+        inputLainnya.style.display = 'block';
+        jumlahManual.required = true;
+      } else {
+        inputLainnya.style.display = 'none';
+        jumlahManual.required = false;
+        jumlahManual.value = '';
+      }
+    });
+  });
+});
