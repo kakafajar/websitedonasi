31a54fa137a5a4335c8256f5f09a61f454e4efa6
irfan-ubpk23 <if23.muhamadfauzi@mhs.ubpkarawang.ac.id>
2024-12-03 01:24:37 +0700

    menambahkan info donasi di dashboard
    Signed-off-by: irfan-ubpk23 <if23.muhamadfauzi@mhs.ubpkarawang.ac.id>

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,21 @@
 <?php
 
+    require_once __DIR__ . '/../models/transaksi.php';
+
     $title = "Dashboard";
+
+    $total_donasi_uang = 0;
+    $total_pending = 0;
+    $total_konfirmasi = 0;
+    
+    $transaksis = Transaksi::get_all();
+    foreach($transaksis as $transaksi){
+        if ($transaksi->get_status() == "finished"){
+            $total_donasi_uang += $transaksi->get_jumlah();
+            $total_konfirmasi += 1;
+        }
+        $total_pending += 1;
+    }
 
     require_once __DIR__ . '/views/layout.php';
     require_once __DIR__ . '/views/index.php';
