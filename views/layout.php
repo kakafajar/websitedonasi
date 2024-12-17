5d5a1dad0e0854039d2b96024db8ee37199cf326
alimedianto <if23.alimedianto@mhs.ubpkarawang.ac.id>
2024-12-17 17:00:06 +0800

    menambahkan scrollbar ke kotak konfirmasi  (responsif)
    Signed-off-by: alimedianto <if23.alimedianto@mhs.ubpkarawang.ac.id>

--- views/layout.php
+++ views/layout.php
@@ -95,19 +95,10 @@
                     <i class="fab fa-facebook-f"></i>
                 </a>
                 <a href="" class="me-4 text-reset">
-                    <i class="fab fa-twitter"></i>
-                </a>
-                <a href="" class="me-4 text-reset">
-                    <i class="fab fa-google"></i>
-                </a>
-                <a href="" class="me-4 text-reset">
                     <i class="fab fa-instagram"></i>
                 </a>
                 <a href="" class="me-4 text-reset">
-                    <i class="fab fa-linkedin"></i>
-                </a>
-                <a href="" class="me-4 text-reset">
-                    <i class="fab fa-github"></i>
+                    <i class="fab fa-tiktok"></i>
                 </a>
             </div>
             <!-- Right -->
@@ -123,12 +114,8 @@
                     <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
                         <!-- Content -->
                         <h6 class="text-uppercase fw-bold mb-4">
-                            <i class="fas fa-gem me-3"></i>Company name
+                            <i class="fas me-3"></i>Saung Do'a Anak Yatim
                         </h6>
-                        <p>
-                            Here you can use rows and columns to organize your footer content. Lorem ipsum
-                            dolor sit amet, consectetur adipisicing elit.
-                        </p>
                     </div>
                     <!-- Grid column -->
 
